a06367fa8ad594625a59e01551f8ed44ef76a220
Nickolas Johnson <nrjohns1@mtu.edu>
2024-12-02 09:26:25 -0500

    beep boop clean code

--- CameraScript.py
+++ CameraScript.py
@@ -1,30 +1,28 @@
 #CameraScript.py
 import cv2
 
+'''Handler file for the camera. Place any camera or device operation functions here'''
 class Camera_Tools:
-    
-    
-    
+       
     def TakePhoto():
-        # Open a connection to the webcam (0 is the default camera)
+        # Open webcam(default camera = 0)
         video_capture = cv2.VideoCapture(0)
 
-        # Check if the webcam is opened correctly
         if not video_capture.isOpened():
             raise Exception("Could not open video device")
 
-        # Read a frame from the webcam
+        # Read a frame from the webcam and release
         ret, frame = video_capture.read()
-
-        # Release the webcam
         video_capture.release()
 
         # Check if the frame was captured successfully
         if ret:
-            # Save the frame as a JPG file
-            cv2.imwrite('captured_image.jpg', frame)
+            cv2.imwrite('captured_image.jpg', frame)                # Save the frame as a JPG file
+            
         else:
             print("Failed to capture image")
+        
+        return 
 
 #Ran when script is run independently. The function should be run in another script            
 Camera_Tools.TakePhoto()
