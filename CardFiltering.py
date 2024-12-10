92cfd3465113436ce4af74720b9d015a40d4f307
nrjohns1 <nrjohns1@mtu.edu>
2024-12-09 19:05:57 -0500

    Filtered images added to database

--- CardFiltering.py
+++ CardFiltering.py
@@ -1,24 +1,29 @@
 #Card Filtering.py
+import pathlib
 import os
+from os import path
 import cv2
 import numpy as np
 
 #Paths
-#img = os.path.relpath(r'./Card_Database/2Club.jpg')
-Card_Database = os.path.relpath(r'./captured_image.jpg')
+Card_Database = r'./Card_Database'
+Card_Outputs = r'./Card_Database/Card_Outputs'
+
+#DEBUG BOOL
+loop_through_files = True
 
 class FilteringTools:
     
     def color2bin(img = ''):
         img_name = img.split(".")[0]
-        
+        input_img = path.join(Card_Database,img)
         ##Mask
         #read image
-        img_grey = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+        img_grey = cv2.imread(input_img, cv2.IMREAD_GRAYSCALE)
         img_bw = cv2.GaussianBlur(img_grey,(5,5),0)
 
         # define a threshold, 128 is the middle of black and white in grey scale
-        thresh = 150
+        thresh = 200
 
         # threshold the image
         img_binary = cv2.threshold(img_bw, thresh, 255, cv2.THRESH_BINARY)[1]
@@ -28,6 +33,13 @@ class FilteringTools:
     def ImgTrim(img_binary):
         
         contours, _ = cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Finds the largest contour (card edge)
+        largest = contours[0]
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > cv2.contourArea(largest):
+                largest = contour
 
         # Get the bounding box of the largest contour
         x, y, w, h = cv2.boundingRect(contours[0])
@@ -43,22 +55,36 @@ class FilteringTools:
         cropped_image = img_binary[y_new : y_new + h_new , x_new : x_new + w_new]
 
         return fitted_image , cropped_image
-'''
-##UNCOMMENT THIS WHEN I CAN FIGURE OUT WINDOWS FILESYSTEM PERMISSIONS
-dataset = open(Card_Database)
 
-for image in dataset:
-    color_image = image
-    img_name,img_binary = FilteringTools.color2bin(color_image)    
-    #save image
-    cv2.imwrite(f'./{img_name}_bw.png',img_binary) 
-'''   
+if loop_through_files is True:
+    counter = 1
+    dataset = os.listdir(Card_Database)
 
-color_image = Card_Database
-img_binary, img_name = FilteringTools.color2bin(color_image)
-cropped_img, trimmed_img = FilteringTools.ImgTrim(img_binary)
+    for image in dataset:
+
+        if image == "Card_Outputs":
+            pass
+        else:
+            #Format image
+            color_image = image
+            img_binary,img_name = FilteringTools.color2bin(color_image)
+            cropped_img, trimmed_img = FilteringTools.ImgTrim(img_binary)  
+
+            #save image
+            cv2.imwrite(path.join(Card_Outputs,f'{img_name}_bin.png'),img_binary) 
+            cv2.imwrite(path.join(Card_Outputs,f'{img_name}_crp.png'),cropped_img) 
+            cv2.imwrite(path.join(Card_Outputs,f'{img_name}_trm.png'),trimmed_img)
+            print(f"Image outputs Written : {counter} of {len(dataset)}")
+            counter += 1
+
+else:
+
+    color_image = Card_Database
+    img_binary, img_name = FilteringTools.color2bin(color_image)
+    cropped_img, trimmed_img = FilteringTools.ImgTrim(img_binary)
     
-#save image
-cv2.imwrite(f'./{img_name}_bin.png',img_binary) 
-cv2.imwrite(f'./{img_name}_crp.png',cropped_img) 
-cv2.imwrite(f'./{img_name}_trm.png',trimmed_img) 
+    #save image
+    cv2.imwrite(path.join(Card_Outputs,f'{img_name}_bin.png'),img_binary) 
+    cv2.imwrite(path.join(Card_Outputs,f'{img_name}_crp.png'),cropped_img) 
+    cv2.imwrite(path.join(Card_Outputs,f'{img_name}_trm.png'),trimmed_img)
+    print("Image outputs Written") 
