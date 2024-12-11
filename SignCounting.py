b77fb7c70e85a3c82be0cd08ef11d9e06761fd25
Holden Sutherland <hcsuther@mtu.edu>
2024-12-11 12:00:34 -0500

    Symbol Counter Update

--- SignCounting.py
+++ SignCounting.py
@@ -2,11 +2,6 @@ import cv2 # type: ignore
 import numpy as np # type: ignore
 import CardClassification as classify
 
-## GOALs 
-# Make something to find the right x values to count that column
-# Then Make something to go down vertically and log when the pixel color changes from black to white?
-    # White to Black? And stop counting when outside of contour so it doesn't count the change on the outer edge
-
 
 class symbolCounter:
 
@@ -72,68 +67,13 @@ class symbolCounter:
 
         return transitions, transitionTotal, img
 
-    def BoundingBox(image_path):
-        img = cv2.imread(image_path)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #not needed if inputted pictures are already made B & W
-        edges = cv2.Canny(gray, 100, 200)
 
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)  # Get bounding box coordinates
-            
-            #removing the too small bounding rectangles
-            if w < 30:
-                w = 0
-            if h < 30:  #30 is arbitrary
-                h = 0
-
-            #Making Final Points
-            xf = x + w
-            yf = y + h
-
-            #Making them integers so they don't have decimal points attached - pixels cant be something.5, must be integer
-            xf = int(xf)
-            yf = int(yf)
-
-            #Checking If they are too small, and then dont write them if so      
-            if w > 0 and h > 0:
-                #cv2.rectangle(image, start_point, end_point, color, thickness)
-                cv2.rectangle(img, (x, y), (xf, yf), (0, 255, 0), 2)  # Draw rectangle
-                
-        return img
-    
-    def NestedContours(image_path):
-        # Read the image
-        img = cv2.imread(image_path) 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                    #Unneeded steps for the already trimmed images
-        _, thresh = cv2.threshold(gray, 175, 255, cv2.THRESH_BINARY)    #Also unneeded for trimmed, just need to update variables later
-        # cv2.threshold(source, thresholdValue, maxVal, thresholdingTechnique) 
-
-        # Find contours with hierarchy
-        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Draw all contours
-        cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
-
-        return img
-
-
-    ## OUT OF THE FUNCTIONS
+    ## OUT OF THE FUNCTION
 
     #Setting up the filepath
     directory = "C:/Users/holds/SPRBlackjackRobot/NED-Blackjack/Card_Database/Card_Outputs"
-    #filename = "10Spade_trm.png"
-    #filename = "5Diamond.jpg"
     filename = "AceSpade_trm.png"
     image_path = directory + "/" + filename
-
-    # Getting the image with bounding boxes
-    # Calcs the width (w) needed for the white_to_black checker!!!
-    imgBound = BoundingBox(image_path)
-
-    # Getting the image with Contours to print
-    imgContours = NestedContours(image_path)
         
     # Getting the locations of the symbols and the image of them to display
     transitions, transitionTotal, imgW2B = WhiteToBlackDetector(image_path)
@@ -142,9 +82,7 @@ class symbolCounter:
 
     print("Card Score: {}".format(classify.ClassificatonTools.card_scoring(transitionTotal)))
 
-    # Display the image with contours
+    # Display the image with transition points marked
     cv2.imshow('Changes in Color Points', imgW2B)
-    #cv2.imshow('BoundingBox', imgBound)
-    #cv2.imshow('Contours', imgContours)  #temporarily suppressed
     cv2.waitKey(0)
     cv2.destroyAllWindows()
